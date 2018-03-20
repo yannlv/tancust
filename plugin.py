fb28b0f3e1efcd4ded144217af95ba8d66934d9e
Yann LV <yann.le.van.ext@mappy.com>
2018-03-20 15:59:43 +0100

    Rolling back reader implementation from plugin.py [DONE] + fixing test execution and completion steps [WIP]

--- plugin.py
+++ plugin.py
@@ -1,52 +1,47 @@
 from pkg_resources import resource_string
-from yandextank.plugins.Aggregator import AbstractReader, AggregatorPlugin, \
-    AggregateResultListener, SecondAggregateDataItem
-from yandextank.plugins.ConsoleOnline import \
-    ConsoleOnlinePlugin, AbstractInfoWidget
-from yandextank.core import AbstractPlugin
-import yandextank.core as tankcore
-import yandextank.plugins.ConsoleScreen as ConsoleScreen
 
-#from ...common.interfaces import AbstractPlugin, AggregateResultListener, AbstractInfoWidget, GeneratorPlugin
+from yandextank.common.interfaces import AbstractPlugin, AggregateResultListener, AbstractInfoWidget, GeneratorPlugin, AbstractCriterion
+#from yandextank.plugins.Console import Plugin as ConsolePlugin
+from yandextank.plugins.Autostop import Plugin as AutostopPlugin
+#from ..Console import Plugin as ConsolePlugin
+#from ..Autostop import Plugin as AutostopPlugin
+from yandextank.plugins.Console import screen as ConsoleScreen
 
+#from ...common.util import splitstring
+from yandextank.common.util import splitstring
+#from .reader import LocustReader, LocustStatsReader
+#from ...stepper import StepperWrapper
+from yandextank.stepper import StepperWrapper
 
+from locust import runners as lr
 from locust import main as lm
 from locust import log as ll
 import locust
 
 
-from .reader import LocustReader, LocustStatsReader
-
-from locust import runners
-
 import gevent
 import sys
-#import os
-#import signal
-#import inspect
 import logging
-#import socket
-#from optparse import OptionParser
-#
-#import locust.web 
-#from locust.log import setup_logging, setup_resplogging, console_logger
 from locust.stats import stats_printer, print_percentile_stats, print_error_report, print_stats
-#from locust.inspectlocust import print_task_ratio, get_task_ratio_dict
-#from locust.core import Locust, HttpLocust
-from locust.runners import MasterLocustRunner, SlaveLocustRunner, LocalLocustRunner
+from  locust.runners import MasterLocustRunner, SlaveLocustRunner, LocalLocustRunner
 import locust.events as events
+from locust.util.time import parse_timespan
 
 #_internals = [Locust, HttpLocust]
 version = locust.__version__
 
-class LocustPlugin(AbstractPlugin, GeneratorPlugin):
+logger = logging.getLogger(__name__)
+
+class Plugin(AbstractPlugin, GeneratorPlugin):
 
     """ Locust tank plugin """
     SECTION = 'locust'
 
 
-    def __init__(self, core):
-        AbstractPlugin.__init__(self, core)
+    def __init__(self, core, cfg, cfg_updater):
+        AbstractPlugin.__init__(self, core, cfg, cfg_updater)
+ #       self.stats_reader = None
+ #       self.reader = None
         self.host = None
         self.web_host = ""
         self.port = 8089
@@ -61,6 +56,7 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
         self.num_clients = int(1)
         self.hatch_rate = float(1)
         self.num_requests = None
+        self.run_time = '60s'
         self.loglevel = 'INFO'
         self.logfile = None
         self.csvfilebase = None
@@ -74,18 +70,18 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
         self.locustlogfile = False
         self.locustloglevel = 'INFO'
 
+
         # setup logging
         ll.setup_logging(self.loglevel, self.logfile)
         if self.locustlogfile:
             ll.setup_resplogging(self.locustloglevel, self.locustlogfile)
 
-        logger = logging.getLogger(__name__)
 
 
     def get_available_options(self):
         return [
             "host", "port", "locustfile",
-            "num_clients", "hatch_rate", "num_requests",
+            "num_clients", "hatch_rate", "run_time", #"num_requests",
             "logfile", "loglevel", "csvfilebase"
         ]
 
@@ -98,6 +94,15 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
         logging.debug("Variables: %s", res)
         return res
 
+#    def get_reader(self):
+#        if self.reader is None:
+#            self.reader = LocustReader(self.locustlogfile)
+#        return self.reader
+#
+#    def get_stats_reader(self):
+#        if self.stats_reader is None:
+#            self.stats_reader = self.reader.stats_reader
+#        return self.stats_reader
 
     def configure(self):
         self.host = self.get_option("host")
@@ -105,13 +110,19 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
         self.locustfile = self.get_option("locustfile")
         self.num_clients = int(self.get_option ("num_clients"))
         self.hatch_rate = float(self.get_option("hatch_rate"))
-        self.num_requests = int(self.get_option("num_requests"))
+        #self.num_requests = int(self.get_option("num_requests"))
+        self.run_time = self.get_option("run_time")
         self.logfile = self.get_option("logfile")
         self.loglevel = self.get_option("loglevel")
         self.csvfilebase = self.get_option("csv")
         self.locustlogfile = self.get_option("locustlogfile")
         self.locustloglevel = self.get_option("locustloglevel")
-
+#        try:
+#            autostop = self.core.get_plugin_of_type(AutostopPlugin)
+#            autostop.add_criterion_class(UsedInstancesCriterion)
+#        except KeyError:
+#            logging.debug(
+#                "No autostop plugin found, not adding instances criterion")
 
     def get_options(self):
         options = {optname : self.__getattribute__(optname) for optname in self.get_available_options()}
@@ -120,29 +131,18 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
 
     def prepare_test(self):
         logger = logging.getLogger(__name__)
-        aggregator = None
-        try:
-            aggregator = self.core.get_plugin_of_type(AggregatorPlugin)
-        except Exception as ex:
-            logger.warning("No aggregator found: %s", ex)
+#        aggregator = None
 
-        if aggregator:
-            aggregator.reader = LocustReader(self.locustlogfile)
-            aggregator.stats_reader = aggregator.reader.stats_reader
+#        try:
+#            console = self.core.get_plugin_of_type(ConsolePlugin)
+#        except Exception as ex:
+#            logger.debug("Console not found: %s", ex)
+#            console = None
 
-        try:
-            console = self.core.get_plugin_of_type(ConsolePlugin)
-        except Exception as ex:
-            logger.debug("Console not found: %s", ex)
-            console = None
-
-        if console:
-            widget = JMeterInfoWidget(self)
-            console.add_info_widget(widget)
-            if aggregator:
-                aggregator.add_result_listener(widget)
-
-        return None
+#       if console:
+#            widget = LocustInfoWidget(self)
+#            console.add_info_widget(widget)
+#            self.core.job.aggregator.add_result_listener(widget)
 
 
     def start_test(self):
@@ -167,7 +167,6 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
 
             docstring, locusts = lm.load_locustfile(locustfile)
 
-            logger.info("Locust plugin: locustfile = {}".format(locustfile))
             logging.info("Locust plugin: locustfile = {}".format(locustfile))
 
 
@@ -187,23 +186,78 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
                 logger.info("Locust plugin: Locust classes found in {} : {}".format(locustfile, locusts))
 
             locust_classes = list(locusts.values())
+            #locust_classes = locusts
             options = Opts(**self.get_options())
             print "main() : options = {}".format(options)
 
             # run that shit
-            runners.locust_runner = LocalLocustRunner(locust_classes, options)
+            if self.run_time:
+                if not self.no_web:
+                    logger.error("The --run-time argument can only be used together with --no-web")
+                    sys.exit(1)
+                try:
+                    self.run_time = parse_timespan(self.run_time)
+                except ValueError:
+                    logger.error("Valid --time-limit formats are: 20, 20s, 3m, 2h, 1h20m, 3h30m10s, etc.")
+                    sys.exit(1)
+                def spawn_run_time_limit_greenlet():
+                    logger.info("Run time limit set to %s seconds" % self.run_time)
+                    def timelimit_stop():
+                        logger.info("Time limit reached. Stopping Locust.")
+                        lr.locust_runner.quit()
+                    gevent.spawn_later(self.run_time, timelimit_stop)
+
+            if not self.no_web and not self.slave:
+                # spawn web greenlet
+                logger.info("Starting web monitor at %s:%s" % (self.web_host or "*", self.port))
+                main_greenlet = gevent.spawn(web.start, locust_classes, options)
+
+
+            if not self.master and not self.slave:
+                logger.info("Locust plugin : LocalLocustRunner about to be launched")
+                lr.locust_runner = LocalLocustRunner(locust_classes, options)
+                # spawn client spawning/hatching greenlet
+                if self.no_web:
+                    logger.info("Locust plugin : LocalLocustRunner.start_hatching()")
+                    lr.locust_runner.start_hatching(wait=True)
+                    main_greenlet = lr.locust_runner.greenlet
+                if self.run_time:
+                    logger.info("Locust plugin : spawn_run_time_limit_greenlet()")
+                    spawn_run_time_limit_greenlet()
+            elif self.master:
+                lr.locust_runner = MasterLocustRunner(locust_classes, options)
+                if self.no_web:
+                    while len(lr.locust_runner.clients.ready)<self.expect_slaves:
+                        logging.info("Waiting for slaves to be ready, %s of %s connected",
+                                     len(lr.locust_runner.clients.ready), self.expect_slaves)
+                        time.sleep(1)
+
+                    lr.locust_runner.start_hatching(self.num_clients, self.hatch_rate)
+                    main_greenlet = lr.locust_runner.greenlet
+                    if self.run_time:
+                        spawn_run_time_limit_greenlet()
+            elif self.slave:
+                if self.run_time:
+                    logger.error("--run-time should be specified on the master node, and not on slave nodes")
+                    sys.exit(1)
+                try:
+                    lr.locust_runner = SlaveLocustRunner(locust_classes, options)
+                    main_greenlet = lr.locust_runner.greenlet
+                except socket.error as e:
+                    logger.error("Failed to connect to the Locust master: %s", e)
+                    sys.exit(-1)
 
             # spawn client spawning/hatching greenlet
-            runners.locust_runner.start_hatching(wait=True)
-            main_greenlet = runners.locust_runner.greenlet
+            #lr.locust_runner.start_hatching(wait=True)
+            #main_greenlet = lr.locust_runner.greenlet
             # spawn stats printing greenlet
             #if not options.only_summary and (options.print_stats or (options.no_web and not options.slave)):
-            if not self.only_summary and (self.print_stats or (self.no_web and not self.slave)):
-                gevent.spawn(stats_printer)
-                print('### len(request_stats) = ' + str(len(runners.locust_runner.request_stats)) + '\n')
+            #if not self.only_summary and (self.print_stats or (self.no_web and not self.slave)):
+            #    gevent.spawn(stats_printer)
+            #    print('### len(request_stats) = ' + str(len(lr.locust_runner.request_stats)) + '\n')
 
         except Exception as e:
-            logger.error("{}".format(e))
+            logger.error("### CRITICAL ERROR : %s", e)
             sys.exit(1)
 
 
@@ -214,8 +268,8 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
             logger.info("Locust plugin: Shutting down (exit code %s), bye." % code)
 
             events.quitting.fire()
-            print_stats(runners.locust_runner.request_stats)
-            print_percentile_stats(runners.locust_runner.request_stats)
+            print_stats(lr.locust_runner.request_stats)
+            print_percentile_stats(lr.locust_runner.request_stats)
 
             print_error_report()
             sys.exit(code)
@@ -230,7 +284,7 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
             logger.info("Locust plugin: Starting Locust %s" % version)
             main_greenlet.join()
             code = 0
-            if len(runners.locust_runner.errors):
+            if len(lr.locust_runner.errors):
                 code = 1
                 shutdown(code=code)
         except KeyboardInterrupt as e:
@@ -238,10 +292,128 @@ class LocustPlugin(AbstractPlugin, GeneratorPlugin):
 
 
 
+    def is_test_finished(self):
+        logger.info("Locust plugin: fetching locust status")
+        return 0
+#        if lr.locust_runner.user_count() > 1:
+#            return -1
+#        else:
+#            logger.info("Locust finished")
+#            return 0
 
-
+    def end_test(self, retcode):
+        if lr.locust_runner.user_count > 1:
+            logger.info("Terminating Locust")
+            shutdown(code=0)
+        else:
+            logger.info("Locust has been terminated")
+        return retcode
 
 class Opts:
     def __init__(self, **entries):
         self.__dict__.update(entries)
         self.no_reset_stats = False
+        self.reset_stats = True
+
+
+class LocustInfoWidget(AbstractInfoWidget, AggregateResultListener):
+    """ Right panel widget with Locust test info """
+
+    def __init__(self, locust):
+        AbstractInfoWidget.__init__(self)
+        self.krutilka = ConsoleScreen.krutilka()
+        self.locust = locust
+        self.active_threads = 0
+        self.RPS = 0
+
+    def get_index(self):
+        return 0
+
+    def on_aggregated_data(self, data, stats):
+        self.active_threads = stats['metrics']['instances']
+        self.RPS = data['overall']['interval_real']['len']
+
+
+
+class UsedInstancesCriterion(AbstractCriterion):
+    """
+    Autostop criterion, based on active instances count
+    """
+    RC_INST = 24
+
+    @staticmethod
+    def get_type_string():
+        return 'instances'
+
+    def __init__(self, autostop, param_str):
+        AbstractCriterion.__init__(self)
+        self.seconds_count = 0
+        self.autostop = autostop
+        self.threads_limit = 1
+
+        level_str = param_str.split(',')[0].strip()
+        if level_str[-1:] == '%':
+            self.level = float(level_str[:-1]) / 100
+            self.is_relative = True
+        else:
+            self.level = int(level_str)
+            self.is_relative = False
+        self.seconds_limit = expand_to_seconds(param_str.split(',')[1])
+
+        try:
+            locust = autostop.core.get_plugin_of_type(Plugin)
+            info = locust.get_info()
+            if info:
+                self.threads_limit = info.instances
+            if not self.threads_limit:
+                raise ValueError(
+                    "Cannot create 'instances' criterion"
+                    " with zero instances limit")
+        except KeyError:
+            logger.warning("No locust module, 'instances' autostop disabled")
+
+    def notify(self, data, stat):
+        threads = stat["metrics"]["instances"]
+        if self.is_relative:
+            threads = float(threads) / self.threads_limit
+        if threads > self.level:
+            if not self.seconds_count:
+                self.cause_second = (data, stat)
+
+            logger.debug(self.explain())
+
+            self.seconds_count += 1
+            self.autostop.add_counting(self)
+            if self.seconds_count >= self.seconds_limit:
+                return True
+        else:
+            self.seconds_count = 0
+
+        return False
+
+    def get_rc(self):
+        return self.RC_INST
+
+    def get_level_str(self):
+        """
+        String value for instances level
+        """
+        if self.is_relative:
+            level_str = str(100 * self.level) + "%"
+        else:
+            level_str = self.level
+        return level_str
+
+    def explain(self):
+        items = (
+            self.get_level_str(), self.seconds_count,
+            self.cause_second[0].get('ts'))
+        return (
+            "Testing threads (instances) utilization"
+            " higher than %s for %ss, since %s" % items)
+
+    def widget_explain(self):
+        items = (self.get_level_str(), self.seconds_count, self.seconds_limit)
+        return "Instances >%s for %s/%ss" % items, float(
+            self.seconds_count) / self.seconds_limit
+
--- reader.py
+++ reader.py
@@ -3,6 +3,7 @@ Locust resplog reader. Read chunks from resplog and produce data frames
 """
 import pandas as pd
 import numpy as np
+import queue as q
 import logging
 import json
 import time
@@ -10,76 +11,140 @@ import datetime
 import itertools as itt
 from StringIO import StringIO
 
+from yandextank.aggregator import TimeChopper
+from yandextank.aggregator import aggregator as agg
+
+
 logger = logging.getLogger(__name__)
 
-locout_columns = [
+locust_log_columns = [
     'send_ts', 'host_loglevel_logger', 'tag', 'http_method', 'http_code', 'url',
     'resp_time', 'content_size'
 ]
 
 dtypes = {
     'send_ts': np.str,
-    'host_loglevel': np.str,
+    'host_loglevel_logger': np.str,
     'tag': np.str,
     'http_method': np.str,
-    'http_code': np.int64,
+    #'http_code': np.uint16,
+    'http_code': np.str,
     'url': np.str,
-    'resp_time': np.int64,
-    'content_size': np.int64
+    'resp_time': np.float32,
+    #'content_size': np.int64
+    'content_size': np.str
 }
 
 
 def string_to_df(data):
     start_time = time.time()
-    chunk = pd.read_csv(
-        StringIO(data), sep='\t', names=locout_columns, dtype=dtypes)
+
+    # DEBUG
+    #logger.info("####### DATA : {}".format(data))
+    #logger.info("####### STRINGIO(DATA) : {}".format(StringIO(data)))
+    chunk = pd.read_csv(StringIO(data), sep='\t', names=locust_log_columns, dtype=dtypes, engine='python')
+
+    # DEBUG
+    logger.debug("\n\n####### Chunk :\n{}\n\n".format(chunk))
 
     # format locust log date to timestamp : '[2017-12-28 14:46:34,327]' -> 1514468794.327
-    locout_dt_obj = datetime.strptime(chunk.send_ts.replace('[','').replace(']',''), '%Y-%m-%d %H:%M:%S,%f')
-    chunk['ts'] = time.mktime(locout_dt_obj.timetuple()) + locout_dt_obj.microsecond / 1e3
+    locust_log_dt_obj = datetime.datetime.strptime(chunk.send_ts[0].replace('[','').replace(']',''), '%Y-%m-%d %H:%M:%S,%f')
+    chunk['ts'] = time.mktime(locust_log_dt_obj.timetuple()) + locust_log_dt_obj.microsecond / 1e3
+    chunk['test'] = chunk.http_code.astype(np.str) + chunk.http_method
+    logger.debug("\n\n####### Chunk :\n{}\n\n".format(chunk))
 
     ##chunk['receive_ts'] = chunk.send_ts + chunk.interval_real / 1e6
     chunk['receive_sec'] = chunk.ts.astype(np.int64)
 
     # split host_loglevel in (host, loglevel, logger) : 'localhost/INFO/resplog' -> ('localhost','INFO','resplog')
-    (chunk['host'], chunk['loglevel'], chunk['logger']) = tuple(chunk.host_loglevel.rsplit('/'))
+    [chunk['host'], chunk['loglevel'], chunk['logger']] = chunk.host_loglevel_logger.str.rsplit('/')[0]
 
     #chunk['tag'] = chunk.tag.str.rsplit('#', 1, expand=True)[0]
     chunk.set_index(['receive_sec'], inplace=True)
 
     logger.debug("Chunk decode time: %.2fms", (time.time() - start_time) * 1000)
+    logger.debug("\n####### Chunk : {}".format(chunk))
     return chunk
 
 
 class LocustReader(object):
-    def __init__(self, filename, cache_size=1024 * 1024 * 50):
-        self.buffer = ""
-        self.locout = open(filename, 'r')
-        self.closed = False
-        self.cache_size = cache_size
 
-    def _read_locout_chunk(self):
-        data = self.locout.read(self.cache_size)
+    def __init__(self, filename):
+        self.buffer = ""
+        self.stat_buffer = ""
+        self.locust_log = filename
+        self.locust_finished = False
+        self.agg_finished = False
+        self.closed = False
+        self.stat_queue = q.Queue()
+        self.stats_reader = LocustStatAggregator(
+            TimeChopper(self._read_stat_queue(), 300000000))
+
+    def _read_stat_queue(self):
+        print("############ self.closed : {}".format(self.closed))
+        print("############ self.stat_queue.qsize() : {}".format(self.stat_queue.qsize()))
+        while not self.closed:
+            for _ in range(self.stat_queue.qsize()):
+                print("###########  stat_queue.qsize() = {}".format(self.stat_queue.qsize()))
+                try:
+                    print("########### in the try")
+                    si = self.stat_queue.get_nowait()
+                    if si is not None:
+                        yield si
+                        print("########### in the try/yield")
+                except q.Empty:
+                    print("########### in the except")
+                    break
+
+    def _read_locust_log_chunk(self, locust_log):
+        data = locust_log.read(1024 * 1024 * 10)
         if data:
             parts = data.rsplit('\n', 1)
             if len(parts) > 1:
                 ready_chunk = self.buffer + parts[0] + '\n'
                 self.buffer = parts[1]
-                return string_to_df(ready_chunk)
+                df = string_to_df(ready_chunk)
+                self.stat_queue.put(df)
+                return df
             else:
                 self.buffer += parts[0]
         else:
-            self.buffer += self.locout.readline()
+            if self.locust_finished:
+                self.agg_finished = True
+            locust_log.readline()
         return None
 
     def __iter__(self):
-        while not self.closed:
-            yield self._read_locout_chunk()
-        yield self._read_locout_chunk()
-        self.locout.close()
+        with open(self.locust_log, 'r') as locust_log:
+            while not self.closed:
+                yield self._read_locust_log_chunk(locust_log)
+            yield self._read_locust_log_chunk(locust_log)
 
     def close(self):
         self.closed = True
+
+
+
+
+class LocustStatAggregator(object):
+    def __init__(self, source):
+        self.worker = agg.Worker({"allThreads": ["max"]}, False)
+        self.source = source
+
+    def __iter__(self):
+        for ts, chunk in self.source:
+            stats = self.worker.aggregate(chunk)
+            yield [{
+                'ts': ts,
+                'metrics': {
+                    'instances': stats['allThreads']['max'],
+                    'reqps': 0
+                }
+            }]
+
+    def close(self):
+        pass
+
 
 
 class LocustStatsReader(object):
